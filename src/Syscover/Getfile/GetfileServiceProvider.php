193955a447b5f146ca897128006e88cbc9dc72c0
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-02-19 20:25:05 +0100

    js and service provider

---
+++ src/Syscover/Getfile/GetfileServiceProvider.php
@@ -0,0 +1,55 @@
+<?php namespace Syscover\Forms;
+
+use Illuminate\Support\ServiceProvider;
+
+class GetfileServiceProvider extends ServiceProvider
+{
+	/**
+	 * Bootstrap the application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		// include route.php file
+		include realpath(__DIR__ . '/../../routes.php');
+
+		// register views
+		$this->loadViewsFrom(realpath(__DIR__ . '/../../views'), 'getfile');
+
+        // register translations
+        $this->loadTranslationsFrom(realpath(__DIR__ . '/../../lang'), 'getfile');
+
+		// register public files
+		$this->publishes([
+			realpath(__DIR__ . '/../../../public') => public_path('/packages/syscover/getfile')
+		]);
+
+		// register config files
+		$this->publishes([
+			realpath(__DIR__ . '/../../config/getfile.php') 	=> config_path('getfile.php')
+		]);
+
+        // register migrations
+        $this->publishes([
+            __DIR__.'/../../database/migrations/' 			=> base_path('/database/migrations'),
+			__DIR__.'/../../database/migrations/updates/' 	=> base_path('/database/migrations/updates'),
+        ], 'migrations');
+
+        // register migrations
+        $this->publishes([
+            __DIR__.'/../../database/seeds/' => base_path('/database/seeds')
+        ], 'seeds');
+	}
+
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+        //
+	}
+
+}
