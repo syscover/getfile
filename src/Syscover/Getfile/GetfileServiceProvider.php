802baad2a4f2a050610a419904abf7940ad07243
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-03-05 11:46:52 +0100

    update service provider

--- src/Syscover/Getfile/GetfileServiceProvider.php
+++ src/Syscover/Getfile/GetfileServiceProvider.php
@@ -12,34 +12,24 @@ class GetfileServiceProvider extends ServiceProvider
 	public function boot()
 	{
 		// include route.php file
-		include realpath(__DIR__ . '/../../routes.php');
+		if (!$this->app->routesAreCached())
+			require __DIR__ . '/../../routes.php';
 
 		// register views
-		$this->loadViewsFrom(realpath(__DIR__ . '/../../views'), 'getfile');
+		$this->loadViewsFrom(__DIR__ . '/../../views', 'getfile');
 
         // register translations
-        $this->loadTranslationsFrom(realpath(__DIR__ . '/../../lang'), 'getfile');
+        $this->loadTranslationsFrom(__DIR__ . '/../../lang', 'getfile');
 
 		// register public files
 		$this->publishes([
-			realpath(__DIR__ . '/../../../public') => public_path('/packages/syscover/getfile')
+			__DIR__ . '/../../../public' 			=> public_path('/packages/syscover/getfile')
 		]);
 
 		// register config files
 		$this->publishes([
-			realpath(__DIR__ . '/../../config/getfile.php') 	=> config_path('getfile.php')
+			__DIR__ . '/../../config/getfile.php' 	=> config_path('getfile.php')
 		]);
-
-        // register migrations
-        $this->publishes([
-            __DIR__.'/../../database/migrations/' 			=> base_path('/database/migrations'),
-			__DIR__.'/../../database/migrations/updates/' 	=> base_path('/database/migrations/updates'),
-        ], 'migrations');
-
-        // register migrations
-        $this->publishes([
-            __DIR__.'/../../database/seeds/' => base_path('/database/seeds')
-        ], 'seeds');
 	}
 
 	/**
@@ -51,5 +41,4 @@ class GetfileServiceProvider extends ServiceProvider
 	{
         //
 	}
-
 }
